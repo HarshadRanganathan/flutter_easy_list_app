14ef5a3dd7709e05f947f4043573e1d38d6c5a51
RanganathanH <ranganathanh@dnb.com>
2019-07-03 16:17:48 +0100

    Media query added

--- lib/main.dart
+++ lib/main.dart
@@ -39,6 +39,7 @@ class _MyAppState extends State<MyApp> {
         brightness: Brightness.light,
         primarySwatch: Colors.deepOrange,
         accentColor: Colors.deepPurple,
+        buttonColor: Colors.deepPurple,
       ),
       // home: AuthPage(),
       routes: {
